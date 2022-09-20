3d5042b67151beac1e8b4ea1c9c1c32230f124ea
bhandary1419 <bhandarey1419@gmail.com>
2022-09-20 12:21:18 +0000

    updated jenkinsfile and added Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello from Java this is the second");
+        System.out.println("Hello from Java");
     }
 }
