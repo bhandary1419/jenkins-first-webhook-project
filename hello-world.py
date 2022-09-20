00af57e0ce82da656a8dd8c2c90f17e7c8308385
bhandary1419 <bhandarey1419@gmail.com>
2022-09-20 09:49:46 +0000

    updated hello world

--- hello-world.py
+++ hello-world.py
@@ -1,3 +1,4 @@
 print('Hello World for Jenkins Job')
 print('My first python job which is run within Jenkinsfile.')
 print('We are doing jenkings project')
+print('Hello World for Jenkins second Job')
